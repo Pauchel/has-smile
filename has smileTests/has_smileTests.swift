0cc717d93777071fc37cff2b0c280a12480bf952
山田 怜音iPAD <ameamerain0119@icloud.com>
2019-01-30 18:37:16 +0900

    Initial Commit

---
+++ has smileTests/has_smileTests.swift
@@ -0,0 +1,36 @@
+//
+//  has_smileTests.swift
+//  has smileTests
+//
+//  Created by 山田　怜音 on 2019/01/30.
+//  Copyright © 2019 山田　怜音. All rights reserved.
+//
+
+import XCTest
+@testable import has_smile
+
+class has_smileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
